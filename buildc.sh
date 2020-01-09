54e7734ec9073feaeeda9034862bd7abdf4a6da8
David Alvarez <david.alvarez@bsc.es>
2020-01-09 19:07:28 +0100

    matmul

--- buildc.sh
+++ buildc.sh
@@ -7,7 +7,7 @@ fi
 
 mkdir -p build
 
-riscv64-linux-gnu-gcc -march=rv64im -Os -mabi=lp64 -c $1.c -o build/prog.bin
+riscv64-linux-gnu-gcc -march=rv64im -O2 -mabi=lp64 -c $1.c -o build/prog.bin
 riscv64-linux-gnu-gcc -march=rv64im -o build/program.elf build/prog.bin -T link.ld -nostdlib -Wl,--build-id=none
 riscv64-linux-gnu-objcopy -O binary --only-section=.data* --only-section=.text* build/program.elf build/mem.bin
 hexdump -v -e '"%08x\n"' build/mem.bin > build/mem.txt
