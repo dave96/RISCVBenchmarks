0bfb742af25c49d98c2fe9edc5c45588673a5f15
David Alvarez <david.alvarez@bsc.es>
2020-01-10 15:35:06 +0100

    Division

---
+++ division.c
@@ -0,0 +1,12 @@
+long long a[128];
+long long b[128];
+
+int main() {
+  int i;
+  for(i = 0; i < 128; i++)
+    a[i] = i*229238;
+  for(i = 0; i < 128; i++)
+    b[i] = a[i] / 23;
+
+  asm volatile("ecall");
+}
